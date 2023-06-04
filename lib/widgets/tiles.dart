773514e1b2c8f9166987e1cccf2e9226f07ae490
Kartikey <kartikey0807@gmail.com>
2023-06-04 15:55:18 +0530

    Added border to the tiles

--- lib/widgets/tiles.dart
+++ lib/widgets/tiles.dart
@@ -17,65 +17,71 @@ class tiles extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: category.color, borderRadius: BorderRadius.circular(20)),
-      width: double.infinity,
-      height: 100,
-      child: ListTile(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              title,
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400),
-            ),
-            SizedBox(height: 15),
-            Row(
-              children: [
-                SizedBox(
-                  height: 30,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(category.title),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.black), // Add black border
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            color: category.color, borderRadius: BorderRadius.circular(20)),
+        width: double.infinity,
+        height: 100,
+        child: ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(height: 15),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(category.title),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 20),
-                const Icon(
-                  Icons.calendar_month_outlined,
-                  size: 20,
-                ),
-                SizedBox(width: 4),
-                Text(
-                  DateFormat('yyyy-MM-dd').format(date),
-                  style: TextStyle(color: Colors.black),
-                ),
-              ],
-            ),
-          ],
-        ),
-        trailing: Column(
-          children: [
-            SizedBox(
-              height: 6,
-            ),
-            IconButton(
-                onPressed: onRemove,
-                icon: Icon(
-                  Icons.check_box_outline_blank,
-                  size: 40,
-                ))
-          ],
+                  SizedBox(width: 20),
+                  const Icon(
+                    Icons.calendar_month_outlined,
+                    size: 20,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    DateFormat('yyyy-MM-dd').format(date),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          trailing: Column(
+            children: [
+              SizedBox(
+                height: 6,
+              ),
+              IconButton(
+                  onPressed: onRemove,
+                  icon: Icon(
+                    Icons.check_box_outline_blank,
+                    size: 40,
+                  ))
+            ],
+          ),
         ),
       ),
     );
